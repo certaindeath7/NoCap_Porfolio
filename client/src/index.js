c528532cbaa1cb5f854efb2ecef2f9784d992b2d
Duc Nguyen <ducnguyentranphuc98@gmail.com>
2022-04-19 22:17:29 +1000

    inital project setup

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //help to connect to HTMl file
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
