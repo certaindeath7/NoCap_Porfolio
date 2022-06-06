74d84fb5247086d8161f0fe7bc610bae87388856
Duc Nguyen <ducnguyentranphuc98@gmail.com>
2022-06-06 22:11:51 +1000

    finish up Abouts components

---
+++ client/src/components/SocialMediaIcons.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+
+const SocialMediaIcons = () => {
+  return (
+    <div className="app__social">
+      <div>
+        <BsLinkedin />
+      </div>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
--- client/src/containers/About/About.jsx
+++ client/src/containers/About/About.jsx
@@ -1,12 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import './About.scss';
 import { motion } from 'framer-motion';
-
+import { urlFor, client } from '../../client';
 const About = () => {
+  const [abouts, setAbout] = useState([]);
+
+  const fetchData = async (aboutsQuery) => {
+    try {
+      const data = await client.fetch(aboutsQuery);
+      if (data) {
+        setAbout(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // run once when the component loads
+  useEffect(() => {
+    const aboutsQuery = '*[_type == "abouts"]';
+    fetchData(aboutsQuery);
+  }, []);
   return (
     <>
       {/* Fragment shortcut */}
       <h2 className="head-text">This sums up me as an employee</h2>
+      <div className="app__profiles">
+        {abouts.map((content, value) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.7, type: 'tween' }}
+            className="app__profile-item"
+            key={abouts.title + value}
+          >
+            <img src={urlFor(content.imgUrl)} alt={content.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {content.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {content.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
     </>
   );
 };
